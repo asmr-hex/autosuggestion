fd109112ff437620e2915eb1a07e4ac02fa9efc7
asmr-hex <c@polygon.pizza>
2021-02-26 12:24:29 -0500

    setup test infra for testing LookupNode.matchPattern

---
+++ src/lookup.test.ts
@@ -0,0 +1,8 @@
+import { LookupNode } from './lookup'
+
+
+describe('LookupNode', () => {
+    describe('.matchPattern(...)', () => {
+        it.todo('returns no matches, given tokens which do not match in any context')
+    })
+})
